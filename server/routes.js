e57fd7add348de617a6187c3682ce5142c7ee577
Simone Fillion-Raff <sfillionraff@gmail.com>
2021-03-01 19:50:04 -0500

    finished workshop

--- server/routes.js
+++ server/routes.js
@@ -1,20 +1,25 @@
 const router = require("express").Router();
 
+// const { populateSeats } = require("./util/populateSeats.js");
+
+const { getSeats, bookSeat } = require("./handlers.js");
+
 const NUM_OF_ROWS = 8;
 const SEATS_PER_ROW = 12;
 
 // Code that is generating the seats.
 // ----------------------------------
-const seats = {};
-const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
-for (let r = 0; r < row.length; r++) {
-  for (let s = 1; s < 13; s++) {
-    seats[`${row[r]}-${s}`] = {
-      price: 225,
-      isBooked: false,
-    };
-  }
-}
+// const seats = {};
+// const row = ["A", "B", "C", "D", "E", "F", "G", "H"];
+// for (let r = 0; r < row.length; r++) {
+//   for (let s = 1; s < 13; s++) {
+//     seats[`${row[r]}-${s}`] = {
+//       _id: `${row[r]}-${s}`,
+//       price: 225,
+//       isBooked: false,
+//     };
+//   }
+// }
 // ----------------------------------
 //////// HELPERS
 const getRowName = (rowIndex) => {
@@ -40,64 +45,8 @@ const randomlyBookSeats = (num) => {
 
 let state;
 
-router.get("/api/seat-availability", async (req, res) => {
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
+router.get("/api/seat-availability", getSeats);
 
-  return res.json({
-    seats: seats,
-    bookedSeats: state.bookedSeats,
-    numOfRows: 8,
-    seatsPerRow: 12,
-  });
-});
-
-let lastBookingAttemptSucceeded = false;
-
-router.post("/api/book-seat", async (req, res) => {
-  const { seatId, creditCard, expiration } = req.body;
-
-  if (!state) {
-    state = {
-      bookedSeats: randomlyBookSeats(30),
-    };
-  }
-
-  await delay(Math.random() * 3000);
-
-  const isAlreadyBooked = !!state.bookedSeats[seatId];
-  if (isAlreadyBooked) {
-    return res.status(400).json({
-      message: "This seat has already been booked!",
-    });
-  }
-
-  if (!creditCard || !expiration) {
-    return res.status(400).json({
-      status: 400,
-      message: "Please provide credit card information!",
-    });
-  }
-
-  if (lastBookingAttemptSucceeded) {
-    lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
-    return res.status(500).json({
-      message: "An unknown error has occurred. Please try your request again.",
-    });
-  }
-
-  lastBookingAttemptSucceeded = !lastBookingAttemptSucceeded;
-
-  state.bookedSeats[seatId] = true;
-
-  return res.status(200).json({
-    status: 200,
-    success: true,
-  });
-});
+router.post("/api/book-seat", bookSeat);
 
 module.exports = router;
