8a3df47d81344ae205864a159ead68796d3f6598
Aya Sato <ayasatography@icloud.com>
2021-03-01 21:02:06 -0500

    Exercise 3 & 4 completed

--- server/handlers.js
+++ server/handlers.js
@@ -33,4 +33,44 @@ const getSeats = async (req, res) => {
     });
 };
 
-module.exports = { getSeats };
+const updateSeat = async (req, res) => {
+    const { seatId, creditCard, expiration, fullName, email } = req.body;
+    const client = await MongoClient(MONGO_URI, options);
+    await client.connect();
+    const db = client.db("ticket_booker");
+
+    if (!creditCard || !expiration) {
+        return res.status(400).json({
+            status: 400,
+            message: "Please provide credit card information!",
+        });
+    }
+
+    const result = await db.collection("seats").findOne({ _id: seatId });
+
+    if (result.isBooked === true) {
+        return res.status(400).json({
+            message: "This seat has already been booked!",
+        });
+    }
+
+    else if (result.isBooked === false) {
+        const _id = seatId;
+        const query = { _id };
+        const newValues = { 
+            isBooked: true,
+            purchased_by: fullName,
+            client_email: email
+        };
+        try {
+            await db.collection("seats").updateOne(query, { $set: newValues });
+            res.status(200).json({ status: 200, success: true });
+        } catch (err) {
+            console.log(err);
+            res.status(500).json({ status: 500, message: err.message });
+        }
+        client.close();
+    }
+};
+
+module.exports = { getSeats, updateSeat };
