5435370fe4163fbbd8c422a7ff518fec379af579
“Alexandre <Alex_18h@hotmail.com>
2021-04-05 20:47:22 -0400

    m7-2-dbs-mongo-continued

---
+++ server/occupiedSeats.js
@@ -0,0 +1,29 @@
+const { MongoClient } = require("mongodb");
+const assert = require("assert");
+
+require("dotenv").config();
+const { MONGO_URI } = process.env;
+
+const options = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+const occupiedSeats = async (seats) => {
+  const client = await MongoClient(MONGO_URI, options);
+  try {
+    await client.connect();
+    console.log("connected");
+    const db = client.db("dbticket");
+    const seatInfo = Object.values(seats);
+    const result = await db.collection("seats").insertMany(seatInfo);
+    assert.strictEqual(84, result.insertedCount);
+  } catch (error) {
+    console.log(error);
+  } finally {
+    client.close();
+    console.log("disconnected");
+  }
+};
+
+module.exports = { occupiedSeats };
